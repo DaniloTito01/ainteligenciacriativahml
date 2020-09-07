2b34e8f65a2d674e1885164a13ce3fcee3d8813b
Your Name <you@example.com>
2020-09-07 17:16:20 -0300

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/app', function (req, res) {
-  res.send('Hello World!');
-});
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
