fff317fe1463e203914f9b17ff1a7124744fcd48
Your Name <you@example.com>
2020-09-10 00:48:26 -0300

    criando a aplicação

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ var Sequelize  = require('sequelize');
 
 
 
-  var sequelize = new Sequelize('heroku_00a65bf38f2d8c4', 'b7c2d4c822cc9a', '72cd6e07', {
-      host: 'us-cdbr-east-02.cleardb.com',
+  var sequelize = new Sequelize(process.env.DB_BANCO, process.env.DB_NAME, process.env.DB_PASS, {
+      host: process.env.DB_HOST,
       dialect:  'mysql' /* one of | 'mariadb' | 'postgres' | 'mssql' */
     });
 
 
     
-  
+
     sequelize.authenticate().then(function(){
       console.log('OK');
     }).catch(function(err){
